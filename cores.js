d258b0f65fc3e48037d645cd49cdf1bf4c5c9384
Eric J. Duran <eric.duran7@gmail.com>
2012-01-14 12:22:34 -0500

    Basic db creation

--- cores.js
+++ cores.js
@@ -1,3 +1,22 @@
 var sys    = require('sys'),
     sqlite = require('sqlite'),
     git    = require('git');
+
+
+// Set up a sqlite DB
+var db = new sqlite.Database();
+
+//
+
+db.open("cores.db", function (error) {
+  if (error) {
+      console.log("Blah....");
+      throw error;
+  }
+});
+
+// Get the last rev
+
+// Check the git directory exist
+
+// 
