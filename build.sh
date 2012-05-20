8163d30c29dfe2c3cc81c62b2485cc39d45c690e
Eric J. Duran <eric.duran7@gmail.com>
2012-05-20 18:45:35 -0400

    Updating the build script to also update the repos not just clone them

--- build.sh
+++ build.sh
@@ -4,9 +4,17 @@ echo "Updating Sub Repos"
 
 if [ ! -d "./drupal" ]; then
   git clone --branch 8.x http://git.drupal.org/project/drupal.git
+else
+  cd ./drupal
+  git pull
+  cd ../
 fi
 
 if [ ! -d "./pages" ]; then
   git clone --branch gh-pages git@github.com:ericduran/drupalcores.git pages
+else
+  cd ./pages
+  git pull
+  cd ..
 fi
 
