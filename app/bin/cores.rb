d5338eec8ecce033b5f80fed1d8e94f31ff697d0
alexpott <alex.a.pott@gmail.com>
2020-10-29 13:24:29 +0000

    Update cores.rb

--- app/bin/cores.rb
+++ app/bin/cores.rb
@@ -17,7 +17,7 @@ sum = contributors.values.reduce(:+).to_f
 puts ERB.new(DATA.readlines.join, 0, '>').result
 
 time = Time.now()
-description = "A very basic table of all contributors to Drupal 8 Core"
+description = "A very basic table of all contributors to Drupal 9 Core"
 header = ERB.new(File.new("../templates/partials/header.html.erb").read).result(binding)
 footer = ERB.new(File.new("../templates/partials/footer.html.erb").read).result(binding)
 index_template = File.open("../templates/index.html.erb", 'r').read
